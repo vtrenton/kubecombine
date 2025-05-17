f4094b6ef14da6e22160257dfc3c751d29ce6fe0
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-05-16 23:26:52 -0500

    working out a method for testing this
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- cmd/kubecombine/combine_test.go
+++ cmd/kubecombine/combine_test.go
@@ -1,9 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestMain(t *testing.T) {}
+	"k8s.io/client-go/tools/clientcmd/api"
+)
 
-func TestBuildKubeconfig(t *testing.T) {}
+func TestBuildKubeconfig(t *testing.T) {
 
-func TestConverttoYaml(t *testing.T) {}
+}
+
+func TestConverttoYaml(t *testing.T) {
+	t.Run("Bad JSON conversion of input kc", func(t *testing.T) {
+		kubeconfig := &api.Config{
+			APIVersion:     "v1",
+			Kind:           "Config",
+			Clusters:       "{}",
+			AuthInfos:      "{}",
+			Contexts:       "{}",
+			CurrentContext: "{}",
+		}
+		got := convertToYAML()
+	})
+}
